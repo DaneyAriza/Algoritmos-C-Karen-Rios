9cca4843a46a847c289d7477a4afef7829833a24
DaneyAriza <dsarizav@udistrital.edu.co>
2025-10-24 13:17:27 -0500

    2 CORTE

---
+++ Programa_2_7.c
@@ -0,0 +1,16 @@
+//Daney Stiven Ariza Vargas
+//24 de octubre 2025
+
+#include<stdio.h>
+int main()
+{
+if (0) //Aqui siempre tendremos un valor asignado que nos dara una la indicacion sobre si es falso o verdadero, en este caso se ve si es falso o verdadero
+{
+printf("Esta instrucción nunca se ejecuta\n");
+printf("porque la condición siempre es falsa (0).\n");
+}
+if (21)  // al tener un valor distinto a 0 se va a ejecutar el segundo printf y el primero sera real siempre y cuando el valor sea 0
+ 
+printf("Esta instrucción siempre se ejecuta.\n");
+return 0;
+}
